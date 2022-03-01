4d28cce99a8193f1b0fd9e6b437a197c95248b8f
hengky9099 <85815350+hengky9099@users.noreply.github.com>
2022-03-01 19:22:32 +0700

    Add files via upload

---
+++ kampus_merdeka/week_1/Day_3_Basic_Javascript/script.js
@@ -0,0 +1,14 @@
+const firstname = "hengky"
+const lastname = "goh"
+
+const fullname = firstname + ' ' + lastname
+console.log(fullname)
+
+// belajar ttg penggunaan let, var, const
+// penggunaan data type spt string,integer, dll
+//  operator spt modulus, bagi, kali dll
+// comparation spt lebih dari , kecil dari
+// today learn about array method, regex and how to count big O 
+// notation
+
+
